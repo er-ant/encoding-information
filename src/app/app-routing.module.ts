747c455310023e1ccb58ad2768d4c082e9f055d4
er-ant <er.ant14@yandex.ru>
2017-11-21 20:58:54 +0300

    Fix empty path relocating to cesar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/cesar',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   }, {
     path: 'dashboard',
